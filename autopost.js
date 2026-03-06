0200a0f02c3ae0a750030e48709d72189d33591c
vitalii kostuk <36382856+vitali3337@users.noreply.github.com>
2026-03-06 10:30:26 +0200

    Update autopost.js
    function generateAd(property){
    
    return `🏠 ${property.title}
    
    📍 ${property.address}
    
    💰 ${property.price}
    
    📞 777 26536
    
    #Тирасполь #Недвижимость #RealInvest`
    }
    
    module.exports = { generateAd }

--- autopost.js
+++ autopost.js
@@ -1,1 +1,14 @@
+function generateAd(property){
 
+return `🏠 ${property.title}
+
+📍 ${property.address}
+
+💰 ${property.price}
+
+📞 777 26536
+
+#Тирасполь #Недвижимость #RealInvest`
+}
+
+module.exports = { generateAd }
