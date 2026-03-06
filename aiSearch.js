ed92a569ef5b03768c24fb4e622faed4f11cbb3e
vitalii kostuk <36382856+vitali3337@users.noreply.github.com>
2026-03-06 10:29:16 +0200

    Update aiSearch.js
    const fs = require("fs")
    
    function searchProperty(query){
    
    const db = JSON.parse(fs.readFileSync("./db.json"))
    
    const results = db.properties.filter(p=>{
    
    const text = (p.title+" "+p.address+" "+p.price).toLowerCase()
    
    return text.includes(query.toLowerCase())
    
    })
    
    return results.slice(0,3)
    
    }
    
    module.exports = { searchProperty }

--- aiSearch.js
+++ aiSearch.js
@@ -1,1 +1,19 @@
+const fs = require("fs")
 
+function searchProperty(query){
+
+const db = JSON.parse(fs.readFileSync("./db.json"))
+
+const results = db.properties.filter(p=>{
+
+const text = (p.title+" "+p.address+" "+p.price).toLowerCase()
+
+return text.includes(query.toLowerCase())
+
+})
+
+return results.slice(0,3)
+
+}
+
+module.exports = { searchProperty }
